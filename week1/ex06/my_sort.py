0143ae9197788bb91ece82cb4808d4ee884b6068
Ra96miro <100141204+Ra96miro@users.noreply.github.com>
2022-05-19 11:49:19 +0200

    Add files via upload

---
+++ week1/ex06/my_sort.py
@@ -0,0 +1,39 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    my_sort.py                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: gramiro- <gramiro-@student.42roma.it>      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/05/19 11:18:01 by gramiro-          #+#    #+#              #
+#    Updated: 2022/05/19 11:48:37 by gramiro-         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+
+
+d={
+'Hendrix' : '1942',
+'Allman' : '1946',
+'King' : '1925',
+'Clapton' : '1945',
+'Johnson' : '1911',
+'Berry' : '1926',
+'Vaughan' : '1954',
+'Cooder' : '1947',
+'Page' : '1944',
+'Richards' : '1943',
+'Hammett' : '1962',
+'Cobain' : '1967',
+'Garcia' : '1942',
+'Beck' : '1944',
+'Santana' : '1947',
+'Ramone' : '1948',
+'White' : '1975',
+'Frusciante': '1970',
+'Thompson' : '1949',
+'Burton' : '1939',
+}
+
+sort_dictionary= dict(sorted(d.items(), key=lambda t: t[::-1])) 
+for k, v in sort_dictionary.items(): print (k)
